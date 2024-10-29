c652cf91a4e73df344d3de4965592e1d7beef085
Pramatma Vishwakarma <61196674+pramdevgan@users.noreply.github.com>
2024-10-30 01:47:42 +0530

    first_commit

---
+++ Main.java
@@ -0,0 +1,70 @@
+public class Main
+{
+	public static void main(String[] args) {
+		
+		/*
+		for(int i = 0; i <=6; i++) {
+		    System.out.println(i);
+		}
+		*/
+		
+		
+		/*
+		for(int i = 4; i >=1; i = i - 2) {
+		    
+		    
+		    System.out.println("Lamis");
+		}
+		*/
+		
+		
+// 		int n = 5;
+// 		int sum = 1; // 0
+// 		for(int i = 1 /*0*/; i <= n; i++) {
+// 		    sum  = sum * i; // +
+// 		}
+// 		System.out.println(sum);
+
+    /*
+    for(exp1; exp2; exp3) {
+        //  code
+        
+    }
+    */
+    //  for loop is pre-checking of condition
+    
+    /*
+    int i = 1; // 2 6
+    for( ; i<= 5; i = i + 1) {
+        System.out.println("Lamis");
+    }
+    */
+    
+    
+    // int i = 1;
+    // for(/*optional*/ ;i<=4;  /*optional*/) {
+    //     System.out.println("Lamis");
+    //     // i = i + 1
+    // }
+    // // i = i + 1
+    
+    
+    /*
+    int i = 5;
+    for(i = 1; true; i = i + 1) {
+        System.out.println("Lamis");
+    }
+    */
+    
+    /*
+    for( ; ; ) {
+        System.out.println("Lamis");
+    }
+    */
+    
+	
+	
+	}
+
+	
+}
